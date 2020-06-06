5bb9117679bf80f07502f7714137e45990df11bb
Alradweb <alexradin1964@gmail.com>
2020-06-06 10:30:13 +0300

    test docker

--- gatsby-app/src/pages/index.js
+++ gatsby-app/src/pages/index.js
@@ -1,15 +1,14 @@
 import React from "react"
-//import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 //import Image from "../components/image"
 import SEO from "../components/seo"
-//import Button from '@material-ui/core/Button'
+
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-  </Layout>
+    <Layout>
+        <SEO title="Home"/>
+        <h1>Hi people</h1>
+    </Layout>
 )
 
 export default IndexPage
